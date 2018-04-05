f3fcba5eea6ed1f61d21349829119fc128c16330
The Deceased King <31958700+PAPAOWEN@users.noreply.github.com>
2018-04-04 21:38:40 -0400

    HW

---
+++ Chapter 9.py
@@ -0,0 +1,24 @@
+
+def test_tuple(mytuple=(3 ,2)):
+    return mytuple
+thistupl e =(1 ,2)
+print(test_tuple((1 ,2)))
+
+mytuple =("alli", "wison")
+mytuple(0) = "jaxsin"
+
+print(mytuple(0))
+
+tuplist=("Julia", "Roberts", 1967, "Duplicity", 2009, "Actress", "Atlanta, Georgia")
+
+def f(tup):
+    print(tup[0])
+
+f(tuplist)
+
+#YES it is possible
+#Is a pair a generalization of a tuple, or is a tuple a generalization of a pair?
+#A tuple is a generlization of a pair
+
+# Is a pair a kind of tuple, or is a tuple a kind of pair?
+#A pair is a kind of tuple
